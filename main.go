43c31438a7445164daf8b3bfeee24ceaeb5f9af9
Gabriel de Laboulaye <gabriel.de.laboulaye@gmail.com>
2016-10-02 18:06:56 +0200

    Add an entry point to list the content of db
    It displays a table with year, artist and album.

--- main.go
+++ main.go
@@ -1,25 +1,52 @@
 package main
 
 import (
+  "database/sql"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+  _ "github.com/lib/pq"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+var db *sql.DB
+
+func listAlbums(w http.ResponseWriter, r *http.Request) {
+  rows, err := db.Query("SELECT albums.name as album, year, artists.name as artist FROM albums JOIN artists on artists.artist_id=albums.artist_id;")
+  if err != nil {
+    log.Fatal("Error while querying the DB: %v", err.Error())
+  }
+  fmt.Fprintf(w, "year |           artist |            album\n")
+  for rows.Next() {
+    var album string
+    var year int
+    var artist string
+    err = rows.Scan(&album, &year, &artist)
+    if err != nil {
+      log.Fatal("Error while parsing row: %v", err.Error())
+    }
+    fmt.Fprintf(w, "%3v | %16v | %16v\n", year, artist, album)
+  }
 }
 
 func addRoutes() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", helloWorld).Methods("GET")
+	router.HandleFunc("/", listAlbums).Methods("GET")
   return router
 }
 
+func initDB() {
+  pgdb, err := sql.Open("postgres", "user=gabrieldelaboulaye host=localhost dbname=mrankerdb sslmode=disable")
+  if err != nil {
+    log.Fatal("Could not connect to DB: %v", err.Error())
+  } else {
+    db = pgdb
+  }
+}
+
 func main() {
+  initDB()
 	router := addRoutes()
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
