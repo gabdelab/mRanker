bb22cf25175f288faefb27e05828809eba6bf656
Gabriel de Laboulaye <gabriel.de.laboulaye@gmail.com>
2017-07-02 21:57:06 +0200

    Parse xml file with plist package
    Nothing is done yet with the parsed results, the aim
    is to fill the DB.

--- admin/admin.go
+++ admin/admin.go
@@ -1,24 +1,59 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+
+	"howett.net/plist"
 )
 
-func parseXML(filename string) {
+type Song struct {
+	Year      int    `plist:"Year"`
+	Name      string `plist:"Name"`
+	Album     string `plist:"Album"`
+	PlayCount int    `plist:"PlayCount"`
+	Artist    string `plist:"Artist"`
+}
+
+type File struct {
+	Tracks map[string]Song `plist:"Tracks"`
+}
+
+func parseXML(filename string) error {
 	xmlFile, err := os.Open(filename)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
-		return
+		return err
 	}
 	fmt.Println("Successfully opened file")
 	defer xmlFile.Close()
+
+	b, _ := ioutil.ReadAll(xmlFile)
+	r := bytes.NewReader(b)
+	var file File
+
+	decoder := plist.NewDecoder(r)
+	decoder.Decode(&file)
+
+	counter := 0
+	for _, song := range file.Tracks {
+		if counter < 10 {
+			fmt.Printf("%s - %s - %s\n", song.Name, song.Album, song.Artist)
+		}
+		counter++
+	}
+	return nil
 }
 
 func main() {
 	filename := flag.String("itunes_xml", os.Getenv("ITUNES_XML_FILE"), "file path to iTunes XML")
 	flag.Parse()
 
-	parseXML(*filename)
+	if err := parseXML(*filename); err != nil {
+		log.Fatal("could not parse configuration file")
+	}
 }
